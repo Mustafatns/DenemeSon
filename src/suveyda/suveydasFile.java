3eb32c5030d126b160ecb53741d29daf7319ef06
Suveyda <suveydagulec@gmail.com>
2020-11-22 20:52:13 -0500

    new heyooo

--- src/suveyda/suveydasFile.java
+++ src/suveyda/suveydasFile.java
@@ -2,13 +2,12 @@ package suveyda;
 
 public class suveydasFile {
     public static void main(String[] args) {
-<<<<<<< HEAD
+
         System.out.println("dhajk");
-=======
+
         System.out.println("hello filan");
         System.out.println("selam");
 
 
->>>>>>> suveyda
     }
 }
