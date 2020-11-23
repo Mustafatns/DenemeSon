c4cf1f9ad5c355dc1d3765112e4280da3a33e391
Suveyda <suveydagulec@gmail.com>
2020-11-22 20:42:21 -0500

    new commit2

--- src/suveyda/suveydasFile.java
+++ src/suveyda/suveydasFile.java
@@ -3,5 +3,8 @@ package suveyda;
 public class suveydasFile {
     public static void main(String[] args) {
         System.out.println("hello filan");
+        System.out.println("selam");
+
+
     }
 }
